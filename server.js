f274e215e68001d93b2d08782cefc7cabb860909
KwishyWishy <135928187+KwishyWishy@users.noreply.github.com>
2025-04-23 10:21:32 -0400

    readd port

--- server.js
+++ server.js
@@ -240,3 +240,7 @@ app.get(`/${contentType}`, (req, res) => {
 });
 
 // Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
