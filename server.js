f8e291e0a5f66e51b6c85e7fc6c81517740a33f9
KwishyWishy <135928187+KwishyWishy@users.noreply.github.com>
2025-04-23 10:09:18 -0400

    testing

--- server.js
+++ server.js
@@ -240,7 +240,3 @@ app.get(`/${contentType}`, (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
