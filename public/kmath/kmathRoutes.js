6cbd2e9a57126dade6039a7ea04de26793936a2d
KwishyWishy <kapatel21010@gmail.com>
2025-04-25 21:49:26 -0400

    group button fix

--- public/kmath/kmathRoutes.js
+++ public/kmath/kmathRoutes.js
@@ -79,7 +79,7 @@ router.get('/lesson/:id', (req, res) => {
                 <main>
                     <h1>${lesson.lessonTitle}</h1>
                     <p>${lesson.intro}</p>
-                    <iframe id="ytplayer" type="text/html" width="720" height ="405" src="https://www.youtube.com/embed /${lesson.videoCode}" frameborder="0" allowfullscreen></iframe>
+                    <iframe id="ytplayer" type="text/html" width="720" height ="405" src="https://www.youtube.com/embed/${lesson.videoCode}" frameborder="0" allowfullscreen></iframe>
                     <p>${lesson.lessonContent}</p>
                     <h2>Key Points</h2>
                     <div class="lessons-page">
@@ -102,148 +102,22 @@ router.get('/', (req, res) => {
         if (err) {
             return res.status(500).send('Error reading lessons file');
         }
-        const lessons = JSON.parse(lessonsData);
+        const lessons = JSON .parse(lessonsData);
+        const sortedGroups = [...new Set(lessons.map(lesson => lesson.group))].sort();
 
-        fs.readFile(groupsFilePath, 'utf8', (err, groupsData) => {
-            if (err) {
-                return res.status(500).send('Error reading groups file');
-            }
-            const groups = JSON.parse(groupsData);
-
-            const groupMap = {};
-            groups.forEach(group => {
-                groupMap[group.name] = { lessons: [], order: group.order };
-            });
-
-            lessons.forEach((lesson, index) => {
-                if (groupMap[lesson.group]) {
-                    groupMap[lesson.group].lessons.push({ title: lesson.lessonTitle, index });
-                }
-            });
-
-            const sortedGroups = Object.keys(groupMap).sort((a, b) => {
-                return groupMap[a].order - groupMap[b].order;
-            });
-
-            const groupsList = sortedGroups.map(groupName => {
-                const lessonsList = groupMap[groupName].lessons.map(lesson => `
-                    <li><a href="/${contentType}/lesson/${lesson.index}">${lesson.title}</a></li>
-                `).join('');
-                return `
-                    <button onclick="location.href='/${contentType}/group/${groupName}'" class="group-button">${groupName}</button>
-                `;
-            }).join('');
-
-            const mainPage = `
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>${contentType.charAt(0).toUpperCase() + contentType.slice(1)} Lessons</title>
-                    <link rel="icon" type="image/x-icon" href="/images/tricube-education-favicon.png">
-                    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,600;1,600&display=swap" rel="stylesheet">
-                    <link rel="stylesheet" href="/styles.css">
-                    <style>
-                        body {
-                            font-family: 'Nunito', sans-serif;
-                        }
-                        .lessons-page {
-                            padding: 20px;
-                        }
-                        .group-button {
-                            display: block;
-                            padding: 10px;
-                            margin: 10px 0;
-                            background-color: #86BFF3;
-                            border: none;
-                            border-radius: 5px;
-                            font-size: 20px;
-                            cursor: pointer;
-                            transition: background-color 0.3s;
-                        }
-                        .group-button:hover {
-                            background-color: #68b3f7;
-                        }
-                    </style>
-                </head>
-                <body>
-                <header>
-                    <div class="header-left"><a href="/index.html"><img src="/images/tricube-education-logo.png" style="width:145px;height:60px;"alt="TriCube Education"></a></div>
-                    <nav class="header-right">
-                        <ul>
-                            <li class="dropdown">
-                                <button class="nav-button" onclick="location.href='/grade-select.html'">Subjects</button>
-                                <div class="dropdown-content">
-                                    <div class="subject">
-                                        <a href="#">Mathematics</a>
-                                        <div class="course-dropdown">
-                                            <a href="/kmath">Kindergarten</a>
-                                            <a href="/math1">1st Grade</a>
-                                            <a href="/math2">2nd Grade</a>
-                                            <a href="/math3">3rd Grade</a>
-                                            <a href="/math4">4th Grade</a>
-                                            <a href="/math5">5th Grade</a>
-                                            <a href="/math6">6th Grade</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <button class="nav-button" onclick="location.href='/update-log.html'">Updates</button>
-                            </li>
-                            <li>
-                                <button class="nav-button" onclick="location.href='/about.html'">About</ button>
-                            </li>
-                        </ul>
-                    </nav>
-                </header>
-                <main>
-                    <h1>Kindergarten Math Lessons</h1>
-                    <div class="lessons-page">
-                        ${groupsList}
-                    </div>
-                </main>
-                </body>
-            </html>
+        const groupsList = sortedGroups.map(groupName => {
+            return `
+                <button onclick="location.href='/${contentType}/group/${encodeURIComponent(groupName)}'" class="group-button">${groupName}</button>
             `;
+        }).join('');
 
-            res.send(mainPage);
-        });
-    });
-});
-
-router.get('/groups', (req, res) => {
-    fs.readFile(groupsFilePath, 'utf8', (err, data) => {
-        if (err) {
-            return res.status(500).send('Error reading lessons file');
-        }
-        const groups = JSON.parse(data);
-        res.json(groups);
-    });
-});
-
-// Dynamic route for each group
-router.get('/group/:id', (req, res) => {
-    const groupId = req.params.id;
-    fs.readFile(groupsFilePath, 'utf8', (err, data) => {
-        if (err) {
-            return res.status(500).send('Error reading groups file');
-        }
-        const groups = JSON.parse(data);
-        const group = groups[groupId];
-
-        if (!group) {
-            return res.status(404).send('Group not found');
-        }
-
-        const groupPage = `
+        const mainPage = `
             <!DOCTYPE html>
             <html lang="en">
             <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>${groupName} Group</title>
+                <title>${contentType} Lessons</title>
                 <link rel="icon" type="image/x-icon" href="/images/tricube-education-favicon.png">
                 <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,600;1,600&display=swap" rel="stylesheet">
                 <link rel="stylesheet" href="/styles.css">
@@ -280,6 +154,76 @@ router.get('/group/:id', (req, res) => {
                     </nav>
                 </header>
                 <main>
+                    <h1>${contentType} Lessons</h1>
+                    <div class="groups-list">
+                        ${groupsList}
+                    </div>
+                    <a href="/${contentType}">Back to Lessons</a>
+                </main>
+            </body>
+            </html>
+        `;
+        res.send(mainPage);
+    });
+});
+
+// Dynamic route for each group
+router.get('/group/:name', (req, res) => {
+    const groupName = req.params.name; 
+    fs.readFile(groupsFilePath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Error reading groups file');
+        }
+        const groups = JSON.parse(data);
+        const group = groups.find(g => g.name === groupName); 
+
+        if (!group) {
+            return res.status(404).send('Group not found');
+        }
+
+        const groupPage = `
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>${groupName} Group</title>
+                <link rel="icon" type="image/x-icon" href="/images/tricube-education-favicon.png">
+                <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,600;1,600&display=swap" rel="stylesheet">
+                <link rel="stylesheet" href="/styles.css">
+            </head>
+            <body>
+                <header>
+                    <div class="header-left"><a href="/index.html"><img src="/images/tricube-education-logo.png" style="width:145px;height:60px;"alt="TriCube Education"></a></div>
+                    <nav class="header-right">
+                        <ul>
+                            <li class="dropdown">
+                                <button class="nav-button" onclick="location.href='/grade-select.html'">Subjects</button>
+                                <div class="dropdown-content">
+                                    <div class="subject">
+                                        <a href="#">Mathematics</a>
+                                        <div class="course-dropdown">
+                                            <a href="/kmath">Kindergarten</a>
+                                            <a href="/math1">1st Grade</a>
+                                            <a href="/math2 ">2nd Grade</a>
+                                            <a href="/math3">3rd Grade</a>
+                                            <a href="/math4">4th Grade</a>
+                                            <a href="/math5">5th Grade</a>
+                                            <a href="/math6">6th Grade</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <button class="nav-button" onclick="location.href='/update-log.html'">Updates</button>
+                            </li>
+                            <li>
+                                <button class="nav-button" onclick="location.href='/about.html'">About</button>
+                            </li>
+                        </ul>
+                    </nav>
+                </header>
+                <main>
                     <h1>${groupName} Group</h1>
                     <p>Welcome to the ${groupName} group page!</p>
                     <a href="/${contentType}">Back to Lessons</a>
@@ -291,4 +235,4 @@ router.get('/group/:id', (req, res) => {
     });
 });
 
-module.exports = router; // Export the router for use in server.js
+module.exports = router;
